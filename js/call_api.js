6bd86505fa6047c01dca8938a8fb3b9c913235a2
adigreg <ariannadigregorio2020@u.northwestern.edu>
2020-07-04 13:00:15 -0400

    squiggly and learned how to do api call

--- js/call_api.js
+++
@@ -1,7 +0,0 @@
-async function getData() {
-    let response = await fetch('https://api.nasa.gov/planetary/apod?api_key=yab4F9E4bLvK9i0eaPWs0hsWCGT9adBfmcX1y0gR');
-    let result = await response.json();
-    caption = result.title;
-    document.body.innerHTML = <img src={result.url} alt={caption}></img>
-}
-getData().then(result => console.log(result));
